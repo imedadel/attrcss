dc4ffe4f4cad06bc6980555040f05254cb4182b1
Imed Adel <aadelimed@gmail.com>
2020-03-17 12:10:04 +0100

    :bug: keep extending theme

--- src/generateScreenSpecificCss.js
+++ src/generateScreenSpecificCss.js
@@ -1,17 +1,12 @@
 const { propertyObjectToCss } = require("./propertyObjectToCss");
+const { getPropertiesArray } = require("./getPropertiesArray");
 
 function generateScreenSpecificCss({ separator, parsedTheme, screen = null }) {
   let generatedCss = ``;
   if (!!screen) {
     generatedCss += `@media (min-width: ${screen[1]}) {\n`;
   }
-  [
-    { css: "color", source: parsedTheme.colors },
-    { css: "border-color", source: parsedTheme.colors },
-    { css: "background-color", source: parsedTheme.colors },
-    { css: "fill", source: parsedTheme.colors },
-    { css: "stroke", source: parsedTheme.colors }
-  ].forEach(({ css, source, data }) => {
+  getPropertiesArray(parsedTheme).forEach(({ css, source, data }) => {
     generatedCss += propertyObjectToCss({
       cssProperty: css,
       sourceObject: source,
